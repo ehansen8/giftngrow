6622947ebd34755fd7d9e9973f0252ef916cb4cc
Evan <ehansen8@wisc.edu>
2023-01-25 14:01:43 -0700

    fixed bad api route name

--- src/services/confirmUser.tsx
+++ src/services/confirmUser.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export async function confirmUser(params: { email: string; code: string }) {
-  const { data } = await axios.get(`/api/cognito/signup/confirm`, {
+  const { data } = await axios.get(`/api/cognito/signup/confirmUser`, {
     params: { ...params },
   })
   return data
