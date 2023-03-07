4d85e0075fe424b6e8b98266800d1a059cd61f6f
Evan <ehansen8@wisc.edu>
2023-03-07 11:43:13 -0700

    Why create an account formatting

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -8,6 +8,11 @@ import {
   Link,
   Box,
 } from '@mui/material'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import CheckIcon from '@mui/icons-material/Check'
 import dynamic from 'next/dynamic'
 import { signIn } from 'next-auth/react'
 import { FormEvent, useState } from 'react'
@@ -16,11 +21,17 @@ import { useRouter } from 'next/router'
 import { sendRecoveryCode } from '../../services/sendRecoveryCode'
 import { sendCofirmationCode } from '../../services/sendConfirmationCode'
 
+const bullets = [
+  "Never miss a beat: Each time your item is passed on, you'll receive an email notification that it's traveled.",
+  "Keep tabs on its journey: With a simple click on a link, you can view a detailed log of your item's journey.",
+  'Stay organized: Keep all your tracked items in one convenient location by creating an account.',
+]
+
 function Login() {
   const buttonWidth = '250'
   return (
     <main
-      className='rounded-md mt-4 flex flex-row'
+      className='rounded-md mt-4 flex flex-row box-border'
       style={{ backgroundColor: 'white' }}
     >
       <Box
@@ -29,68 +40,30 @@ function Login() {
       >
         <Typography
           marginBottom={2}
-          variant='h4'
-          color='white'
-          textAlign='center'
-        >
-          Track Your Wrap
-        </Typography>
-        <Typography
-          color='white'
-          variant='body1'
-          component='div'
-        >
-          <p>
-            At <span className='font-ds text-xl'>Gift 'n Grow</span> we are
-            passionate about helping the environment and bringing people
-            together, one wrap and bag at a time!
-          </p>
-          <p>
-            Imagine the special moments and fond memories accumulated in each
-            wrap or bag as it fulfills a special purpose time and time again.
-            Log in, write a message and watch to see how far your Gift will
-            Grow! Simply enter the code found on the inside of your wrap or bag
-            to see the message, its history and follow its journey. You may
-            write a thank you to the original gift giver.
-          </p>
-        </Typography>
-        Signup Here
-        <Typography
-          marginBottom={2}
-          variant='h6'
+          variant='h5'
           color='white'
           textAlign='center'
         >
           Why create an account?
         </Typography>
-        <Typography>
-          Creating your own account will enable you to receive emails to track
-          your wrap or bags journey. Each time the item is passed on you will
-          receive an e-mail notification that it has traveled. You will be able
-          to click on a link and see the log of its journey. We will not sell or
-          share your email. Please see the privacy policy.
-        </Typography>
-        <Typography>Your Codes</Typography>
-        <Typography>
-          Note: If you do not have an account, this page is designed to use
-          "cookies" to remember the code(s) you registered on your computer.
-          Some browsers and software may delete the cookie used to store this
-          information, so writing your bag codes down helps ensure you're able
-          to follow their trail.
-        </Typography>
-        <Typography>
-          When you have an account, any codes previously entered will no longer
-          appear below until you sign in. Please remember to sign in before you
-          register a wrap or bag or pass it on, so it gets recorded on your
-          account. Here's a list of codes already registered on this computer:
-          Please write down your bag code(s) so you can check back to track
-          their progress.
-        </Typography>
+        <List>
+          {bullets.map((text) => {
+            return (
+              <ListItem>
+                <ListItemIcon>
+                  <CheckIcon color='primary' />
+                </ListItemIcon>
+                <ListItemText
+                  primary={text}
+                  color='primary'
+                  primaryTypographyProps={{ color: 'white' }}
+                />
+              </ListItem>
+            )
+          })}
+        </List>
       </Box>
-      <div
-        className='flex flex-wrap flex-col gap-3 content-center mx-auto p-4'
-        style={{ maxWidth: buttonWidth + 'px' }}
-      >
+      <div className='flex flex-wrap flex-col gap-3 content-center p-4 w-1/2'>
         <Typography
           variant='h5'
           textAlign='center'
