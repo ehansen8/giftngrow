b9a0186f2253a5609eea672f5a1c10e1048f5e1b
Evan <ehansen8@wisc.edu>
2023-03-07 10:22:05 -0700

    Stylized Fonts

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -6,6 +6,7 @@ import {
   Checkbox,
   FormControlLabel,
   Link,
+  Box,
 } from '@mui/material'
 import dynamic from 'next/dynamic'
 import { signIn } from 'next-auth/react'
@@ -19,11 +20,75 @@ function Login() {
   const buttonWidth = '250'
   return (
     <main
-      className='p-4 rounded-md mt-4'
+      className='rounded-md mt-4 flex flex-row'
       style={{ backgroundColor: 'white' }}
     >
+      <Box
+        className='w-1/2 rounded-l-md p-5'
+        sx={{ bgcolor: '#006a68' }}
+      >
+        <Typography
+          marginBottom={2}
+          variant='h4'
+          color='white'
+          textAlign='center'
+        >
+          Track Your Wrap
+        </Typography>
+        <Typography
+          color='white'
+          variant='body1'
+          component='div'
+        >
+          <p>
+            At <span className='font-ds text-xl'>Gift 'n Grow</span> we are
+            passionate about helping the environment and bringing people
+            together, one wrap and bag at a time!
+          </p>
+          <p>
+            Imagine the special moments and fond memories accumulated in each
+            wrap or bag as it fulfills a special purpose time and time again.
+            Log in, write a message and watch to see how far your Gift will
+            Grow! Simply enter the code found on the inside of your wrap or bag
+            to see the message, its history and follow its journey. You may
+            write a thank you to the original gift giver.
+          </p>
+        </Typography>
+        Signup Here
+        <Typography
+          marginBottom={2}
+          variant='h6'
+          color='white'
+          textAlign='center'
+        >
+          Why create an account?
+        </Typography>
+        <Typography>
+          Creating your own account will enable you to receive emails to track
+          your wrap or bags journey. Each time the item is passed on you will
+          receive an e-mail notification that it has traveled. You will be able
+          to click on a link and see the log of its journey. We will not sell or
+          share your email. Please see the privacy policy.
+        </Typography>
+        <Typography>Your Codes</Typography>
+        <Typography>
+          Note: If you do not have an account, this page is designed to use
+          "cookies" to remember the code(s) you registered on your computer.
+          Some browsers and software may delete the cookie used to store this
+          information, so writing your bag codes down helps ensure you're able
+          to follow their trail.
+        </Typography>
+        <Typography>
+          When you have an account, any codes previously entered will no longer
+          appear below until you sign in. Please remember to sign in before you
+          register a wrap or bag or pass it on, so it gets recorded on your
+          account. Here's a list of codes already registered on this computer:
+          Please write down your bag code(s) so you can check back to track
+          their progress.
+        </Typography>
+      </Box>
       <div
-        className='flex flex-wrap flex-col gap-3 content-center m-auto'
+        className='flex flex-wrap flex-col gap-3 content-center mx-auto p-4'
         style={{ maxWidth: buttonWidth + 'px' }}
       >
         <Typography
@@ -99,7 +164,7 @@ const LoginForm = () => {
   }
   return (
     <form
-      className='flex flex-wrap flex-col gap-3 content-center m-auto'
+      className='flex flex-wrap flex-col gap-3 content-center'
       onSubmit={handleSubmit}
     >
       <Typography
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ds: ['Dancing Script', 'cursive'],
+      }
+    },
   },
   plugins: [],
 }
