81bc69ae3b752dfd81a1b352400623528db03994
Evan <ehansen8@wisc.edu>
2023-03-08 18:07:13 -0700

    removed root redirect and changed /tracking to be the new index

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,4 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
-  const url = request.nextUrl.clone()
-  if (url.pathname === '/') {
-    url.pathname = '/tracking'
-    return NextResponse.redirect(url)
-  }
-}
+export function middleware(request: NextRequest) {}
